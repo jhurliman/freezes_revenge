a814003ca685ec59e4bedae649e3c9d33791128a
John Hurliman <john.hurliman@getcruise.com>
2018-08-24 23:35:04 -0700

    Hardcode models, toggle GPIO relay

--- src/freezes_revenge.py
+++ src/freezes_revenge.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python2
+
 from __future__ import print_function
 from os.path import dirname, join
 
@@ -7,13 +9,28 @@ import snowboydecoder
 import sys
 
 
+MODELS = [
+    join(dirname(__file__), '..', 'resources', 'mr_freeze.pmdl'),
+    join(dirname(__file__), '..', 'resources', 'turn_off.pmdl'),
+]
+
 SOUNDS = [
     join(dirname(__file__), '..', 'sounds', 'freeze-is-coming.wav'),
     join(dirname(__file__), '..', 'sounds', 'the-ice-age.wav'),
     join(dirname(__file__), '..', 'sounds', 'winter-has-come-at-last.wav'),
 ]
 
+GPIO_PIN = 23
+
 interrupted = False
+relay = None
+
+try:
+    from gpiozero import LED
+    relay = LED(GPIO_PIN)
+    print('Using GPIO relay on pin {}'.format(GPIO_PIN))
+except:  # noqa
+    print('Skipping GPIO relay')
 
 
 def signal_handler(signal, frame):
@@ -29,28 +46,23 @@ def interrupt_callback():
 def on_detected():
     print('Turning on')
     snowboydecoder.play_audio_file(random.choice(SOUNDS))
-    # TODO
+    if relay:
+        relay.on()
 
 
 def off_detected():
     print('Turning off')
-    # TODO
+    if relay:
+        relay.off()
 
 
 def main():
-    if len(sys.argv) != 3:
-        print("Error: need to specify 2 model names")
-        print("Usage: python demo.py 1st.model 2nd.model")
-        sys.exit(-1)
-
-    models = sys.argv[1:]
-
     # capture SIGINT signal, e.g., Ctrl+C
     signal.signal(signal.SIGINT, signal_handler)
 
-    sensitivity = [0.5]*len(models)
-    detector = snowboydecoder.HotwordDetector(models, sensitivity=sensitivity)
-    print('Listening... Press Ctrl+C to exit')
+    sensitivity = [0.5]*len(MODELS)
+    detector = snowboydecoder.HotwordDetector(MODELS, sensitivity=sensitivity)
+    print('Mr. Freeze is listening...')
 
     detector.start(detected_callback=[on_detected, off_detected],
                    interrupt_check=interrupt_callback,
